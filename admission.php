e38f26dfa15b7bad4fcdf7037fa76e4b1a77d7df
jayanthansenthilkumar <jayanthansenthilkumar18@gmail.com>
2025-08-29 22:03:12 +0530

    Files Modified Fixed

--- admission.php
+++ admission.php
@@ -30,7 +30,7 @@ if($ayear_result) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MKCE Admission Dashboard</title>
+    <title>MKCE | ADMISSION</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if(isset($_SESSION['username']) || isset($_SESSION['id'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MKCE - Admission Portal</title>
+    <title>MKCE | ADMISSION</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <style>
